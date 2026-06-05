570672d621782ece714da7968a5e79a34effda37
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2026-06-05 10:34:14 +0200

    v1.2.3 feat(core): update project files
    Patch Notes:
    * Update bumpver.toml
    * Update leetcrate/__init__.py
    * Update leetcrate/generators/__init__.py
    * Update pyproject.toml

--- leetcrate/generators/__init__.py
+++ leetcrate/generators/__init__.py
@@ -13,7 +13,7 @@ Supported languages: ``python``, ``java``, ``javascript``, ``go``, ``cpp``.
 """
 
 # version for runtime access
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 from .python_generator import generate_boilerplate as generate_python
 from .java_generator import generate_boilerplate as generate_java
 from .javascript_generator import generate_boilerplate as generate_javascript
